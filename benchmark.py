e206fd0a5b0cc8405717820e1818746506ced4f7
Sergey Astanin <s.astanin@gmail.com>
2013-03-11 16:57:55 +0100

    comparative benchmark: join vs csv vs asciitable vs tabulate vs PrettyTable

---
+++ benchmark.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from timeit import timeit
+from tabulate import tabulate
+
+setup_code = r"""
+from csv import writer
+from StringIO import StringIO
+from tabulate import tabulate
+from prettytable import PrettyTable
+from asciitable import write, FixedWidth
+
+table=[["some text"]+range(i,i+9) for i in range(10)]
+
+
+def csv_table(table):
+    buf = StringIO()
+    writer(buf).writerows(table)
+    return buf.getvalue()
+
+
+def join_table(table):
+    return "\n".join(("\t".join(map(str,row)) for row in table))
+
+
+def prettytable(table):
+    pp = PrettyTable()
+    for row in table:
+        pp.add_row(row)
+    return str(pp)
+
+def asciitable(table):
+    buf = StringIO()
+    write(table, output=buf, Writer=FixedWidth)
+    return buf.getvalue()
+
+"""
+
+methods = [("join with tabs and newlines", "join_table(table)"),
+           ("csv to StringIO", "csv_table(table)"),
+           ("asciitable (0.8)", "asciitable(table)"),
+           ("tabulate (0.4)", "tabulate(table)"),
+           ("PrettyTable (0.7.1)", "prettytable(table)"),
+           ]
+
+
+def benchmark(n):
+    results = [(desc, timeit(code, setup_code, number=n)/n * 1e6)
+               for desc, code in methods]
+    mintime = min(map(lambda x: x[1], results))
+    results = [(desc, t, t/mintime) for desc, t in results]
+    print tabulate(results, ["Table formatter", "time, μs", "rel. time"], "rst", floatfmt=".1f")
+
+
+benchmark(1000)
