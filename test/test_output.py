4d55523c80c05a815372a3253216287ca6134844
Sergey Astanin <s.astanin@gmail.com>
2013-10-17 12:35:10 +0200

    output tests for tablefmt=latex

--- test/test_output.py
+++ test/test_output.py
@@ -8,17 +8,32 @@ from tabulate import tabulate
 
 
 def test_latex():
-    "Testing Latex output"
+    "Output: LaTeX with headers"
     ll = [["a","one",1],["b","two",None]]
-    expected = u'0    1      2\n---  ---  ---\na    one    1\nb    two'
     result   = tabulate(ll, headers="keys", tablefmt="latex")
     expected = "\n".join([
-        r"\begin{tabular}{rrr}",
+        r"\begin{tabular}{llr}",
         r"\hline",
         r" 0   & 1   &   2 \\",
         r"\hline",
         r" a   & one &   1 \\",
         r" b   & two &     \\",
+        r"\hline",
+        r"\end{tabular}"])
+    print("Expected:\n%s\n" % expected)
+    print("Got:\n%s\n" % result)
+    assert expected == result
+
+
+def test_latex_headerless():
+    "Output: LaTeX without headers"
+    tbl = [["spam", 1.23],["eggs", 45.6]]
+    result   = tabulate(tbl, tablefmt="latex")
+    expected = "\n".join([
+        r"\begin{tabular}{lr}",
+        r"\hline",
+        r" spam &  1.23 \\",
+        r" eggs & 45.6  \\",
         r"\hline",
         r"\end{tabular}"])
     print("Expected:\n%s\n" % expected)
