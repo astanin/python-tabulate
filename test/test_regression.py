1118868ed33215a324b67c6709d9e51426e78719
Sergey Astanin <s.astanin@gmail.com>
2013-11-18 14:12:03 +0100

    refactor regression tests

--- test/test_regression.py
+++ test/test_regression.py
@@ -9,27 +9,35 @@ from tabulate import tabulate
 
 
 def test_ansi_color_in_table_cells():
-    "ANSI color in table cells (issue #5)."
+    "Regression: ANSI color in table cells (issue #5)."
     colortable = [('test', '\x1b[31mtest\x1b[0m', '\x1b[32mtest\x1b[0m')]
     colorlessheaders = ('test', 'test', 'test')
     formatted = tabulate(colortable, colorlessheaders, 'pipe')
-    expected = u'| test   | test   | test   |\n|:-------|:-------|:-------|\n| test   | \x1b[31mtest\x1b[0m   | \x1b[32mtest\x1b[0m   |'
+    expected = u"\n".join([u'| test   | test   | test   |',
+                           u'|:-------|:-------|:-------|',
+                           u'| test   | \x1b[31mtest\x1b[0m   | \x1b[32mtest\x1b[0m   |'])
     print("expected: %r\n\ngot:      %r\n" % (expected, formatted))
     assert expected == formatted
 
 
 def test_alignment_of_colored_cells():
-    "Align ANSI-colored values as if they were colorless."
+    "Regression: Align ANSI-colored values as if they were colorless."
     colortable = [('test', 42, '\x1b[31m42\x1b[0m'), ('test', 101, '\x1b[32m101\x1b[0m')]
     colorheaders = ('test', '\x1b[34mtest\x1b[0m', 'test')
     formatted = tabulate(colortable, colorheaders, 'grid')
-    expected = u'+--------+--------+--------+\n| test   |   \x1b[34mtest\x1b[0m |   test |\n+========+========+========+\n| test   |     42 |     \x1b[31m42\x1b[0m |\n+--------+--------+--------+\n| test   |    101 |    \x1b[32m101\x1b[0m |\n+--------+--------+--------+'
+    expected = u'\n'.join([u'+--------+--------+--------+',
+                           u'| test   |   \x1b[34mtest\x1b[0m |   test |',
+                           u'+========+========+========+',
+                           u'| test   |     42 |     \x1b[31m42\x1b[0m |',
+                           u'+--------+--------+--------+',
+                           u'| test   |    101 |    \x1b[32m101\x1b[0m |',
+                           u'+--------+--------+--------+'])
     print("expected: %r\n\ngot:      %r\n" % (expected, formatted))
     assert expected == formatted
 
 
 def test_iter_of_iters_with_headers():
-    "Generator of generators with a generator of headers (issue #9)."
+    "Regression: Generator of generators with a generator of headers (issue #9)."
 
     def mk_iter_of_iters():
         def mk_iter():
@@ -43,25 +51,31 @@ def test_iter_of_iters_with_headers():
             yield h
 
     formatted = tabulate(mk_iter_of_iters(), headers=mk_headers())
-    expected = u'  a    b    c\n---  ---  ---\n  0    1    2\n  0    1    2\n  0    1    2'
+    expected = u'\n'.join([u'  a    b    c',
+                           u'---  ---  ---',
+                           u'  0    1    2',
+                           u'  0    1    2',
+                           u'  0    1    2'])
     print("expected: %r\n\ngot:      %r\n" % (expected, formatted))
     assert expected == formatted
 
 
 def test_datetime_values():
-    "Datetime, date, and time values in cells (issue #10)."
+    "Regression: datetime, date, and time values in cells (issue #10)."
     import datetime
     dt = datetime.datetime(1991,2,19,17,35,26)
     d = datetime.date(1991,2,19)
     t = datetime.time(17,35,26)
     formatted = tabulate([[dt, d, t]])
-    expected = u'-------------------  ----------  --------\n1991-02-19 17:35:26  1991-02-19  17:35:26\n-------------------  ----------  --------'
+    expected = u'\n'.join([u'-------------------  ----------  --------',
+                           u'1991-02-19 17:35:26  1991-02-19  17:35:26',
+                           u'-------------------  ----------  --------'])
     print("expected: %r\n\ngot:      %r\n" % (expected, formatted))
     assert expected == formatted
 
 
 def test_simple_separated_format():
-    "simple_separated_format() accepts any separator (fixed in 33c8bc6)"
+    "Regression: simple_separated_format() accepts any separator (issue #12)"
     from tabulate import simple_separated_format
     fmt = simple_separated_format(u"!")
     expected = u'spam!eggs'
