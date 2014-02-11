5d63e56e8b33f99943e5f1127bb773932427d488
Sergey Astanin <s.astanin@gmail.com>
2014-02-11 18:33:47 +0100

    tests: assert_in fallback in common.py

--- test/common.py
+++ test/common.py
@@ -1,7 +1,17 @@
 try:
-    from nose.tools import assert_equal
+    from nose.tools import assert_equal, assert_in
+
+
 except ImportError:
     def assert_equal(expected, result):
         print("Expected:\n%s\n" % expected)
         print("Got:\n%s\n" % result)
         assert expected == result
+
+
+    def assert_in(result, expected_set):
+        nums = xrange(1, len(expected_set)+1)
+        for i, expected in zip(nums, expected_set):
+            print("Expected %d:\n%s\n" % (i, expected))
+        print("Got:\n%s\n" % result)
+        assert result in expected_set
