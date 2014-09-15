3e2e972b9c10f1b03b95a09049224494b3d38808
Sergey Astanin <s.astanin@gmail.com>
2014-09-15 15:55:51 +0200

    assert_raises replacement for nose.tools.assert_raises

--- test/common.py
+++ test/common.py
@@ -1,5 +1,5 @@
 try:
-    from nose.tools import assert_equal, assert_in
+    from nose.tools import assert_equal, assert_in, assert_raises
 
 
 except ImportError:
@@ -15,3 +15,17 @@ except ImportError:
             print("Expected %d:\n%s\n" % (i, expected))
         print("Got:\n%s\n" % result)
         assert result in expected_set
+
+
+    class assert_raises(object):
+        def __init__(self, exception_type):
+            self.watch_exception_type = exception_type
+        def __enter__(self):
+            pass
+        def __exit__(self, exception_type, exception_value, traceback):
+            if isinstance(exception_value, self.watch_exception_type):
+                return True  # suppress exception
+            elif exception_type is None:
+                msg = "%s not raised" % self.watch_exception_type.__name__
+                raise AssertionError(msg)
+            # otherwise propagate whatever other exception is raised
