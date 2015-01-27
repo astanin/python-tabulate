cb1e5482ba0ab38c97fd5884d602eb5b4b7decc4
Sergey Astanin <s.astanin@gmail.com>
2015-01-27 15:37:47 +0100

    test tabulate CLI: --sep option

--- test/test_cli.py
+++ test/test_cli.py
@@ -15,12 +15,12 @@ import tempfile
 from common import assert_equal
 
 
-def write_sample_input(tmpfile):
+def write_sample_input(tmpfile, sep=' '):
     table = "\n".join([
-        'Sun 696000 1.9891e9',
-        'Earth 6371 5973.6',
-        'Moon 1737 73.5',
-        'Mars 3390 641.85'])
+        sep.join(['Sun', '696000', '1.9891e9']),
+        sep.join(['Earth', '6371', '5973.6']),
+        sep.join(['Moon', '1737', '73.5']),
+        sep.join(['Mars', '3390', '641.85'])])
     tmpfile.write(table)
     tmpfile.file.seek(0)
 
@@ -28,9 +28,11 @@ def write_sample_input(tmpfile):
 def get_stdout_and_stderr(cmd):
     x = subprocess.Popen(cmd,
                          stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT)
-    out, _ = x.communicate()
+                         stderr=subprocess.PIPE)
+    out, err = x.communicate()
     out = out.decode("utf-8")
+    if x.returncode != 0:
+        raise IOError(err)
     return out
 
 
@@ -47,10 +49,28 @@ class TemporaryTextFile(object):
 
 
 def test_script_from_file_to_stdout():
-    """CLI: read from file, print to stdout"""
+    """Command line utility: read from file, print to stdout"""
     with TemporaryTextFile() as tmpfile:
         write_sample_input(tmpfile)
         cmd = ["python", "tabulate.py", tmpfile.name]
+        out = get_stdout_and_stderr(cmd)
+        expected = "\n".join([
+                '-----  ------  -------------',
+                'Sun    696000     1.9891e+09',
+                'Earth    6371  5973.6',
+                'Moon     1737    73.5',
+                'Mars     3390   641.85',
+                '-----  ------  -------------'])
+        print("got:     ",repr(out))
+        print("expected:",repr(expected))
+        assert_equal(out.rstrip().splitlines(), expected.rstrip().splitlines())
+
+
+def test_script_sep_option():
+    """Command line utility: --sep option"""
+    with TemporaryTextFile() as tmpfile:
+        write_sample_input(tmpfile, sep=",")
+        cmd = ["python", "tabulate.py", "--sep", ",", tmpfile.name]
         out = get_stdout_and_stderr(cmd)
         expected = "\n".join([
                 '-----  ------  -------------',
