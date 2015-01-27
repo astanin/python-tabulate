a3429970b4ca7cedca04f27a5f66f9cc4138a377
Sergey Astanin <s.astanin@gmail.com>
2015-01-27 16:33:31 +0100

    test tabulate CLI: -f, --format options

--- test/test_cli.py
+++ test/test_cli.py
@@ -33,6 +33,20 @@ SAMPLE_SIMPLE_FORMAT_WITH_HEADERS = "\n".join(
      'Mars          3390   641.85'])
 
 
+SAMPLE_GRID_FORMAT_WITH_HEADERS = "\n".join(
+    ['+----------+----------+---------------+',
+     '| Planet   |   Radius |          Mass |',
+     '+==========+==========+===============+',
+     '| Sun      |   696000 |    1.9891e+09 |',
+     '+----------+----------+---------------+',
+     '| Earth    |     6371 | 5973.6        |',
+     '+----------+----------+---------------+',
+     '| Moon     |     1737 |   73.5        |',
+     '+----------+----------+---------------+',
+     '| Mars     |     3390 |  641.85       |',
+     '+----------+----------+---------------+'])
+
+
 def sample_input(sep=' ', with_headers=False):
     headers = sep.join(['Planet', 'Radius', 'Mass'])
     rows = [sep.join(['Sun', '696000', '1.9891e9']),
@@ -133,6 +147,19 @@ def test_script_sep_option():
         raw_table = sample_input(sep=",")
         out = run_and_capture_stdout(cmd, input=raw_table)
         expected = SAMPLE_SIMPLE_FORMAT
+        print("got:     ",repr(out))
+        print("expected:",repr(expected))
+        assert_equal(out.splitlines(), expected.splitlines())
+
+
+def test_script_format_option():
+    """Command line utility: -f, --format option"""
+    for option in ["-f", "--format"]:
+        cmd = ["python", "tabulate.py", "-1", option, "grid"]
+        raw_table = sample_input(with_headers=True)
+        out = run_and_capture_stdout(cmd, input=raw_table)
+        expected = SAMPLE_GRID_FORMAT_WITH_HEADERS
+        print(out)
         print("got:     ",repr(out))
         print("expected:",repr(expected))
         assert_equal(out.splitlines(), expected.splitlines())
