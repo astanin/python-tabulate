0b1d392ee81389eb3c0f138b65e4457a56b9acfc
Mayukha Vadari <mvadari@ripple.com>
2022-04-14 17:10:17 -0400

    fix test to get around weird precision issues in python2.7

--- test/test_output.py
+++ test/test_output.py
@@ -1429,7 +1429,7 @@ def test_floatfmt_multi():
 
 
 def test_floatfmt_precision():
-    result = tabulate([[99999998999.999980, 1234.5, 1.2345678, "inf"]], floatfmt=".6f", tablefmt="plain")
+    result = tabulate([["99999998999.999980", 1234.5, 1.2345678, "inf"]], floatfmt=".6f", tablefmt="plain")
     expected = "99999998999.999980  1234.500000  1.234568  inf"
     assert_equal(expected, result)
 
