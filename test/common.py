a8672a525e4144840120f275fcafc791728f5e8a
Arpit Jain <arpitjain099@gmail.com>
2023-04-29 18:10:53 +0900

    add codeql file

--- test/common.py
+++ test/common.py
@@ -6,7 +6,6 @@ def assert_equal(expected, result):
     print("Got:\n%s\n" % result)
     assert expected == result
 
-
 def assert_in(result, expected_set):
     nums = range(1, len(expected_set) + 1)
     for i, expected in zip(nums, expected_set):
