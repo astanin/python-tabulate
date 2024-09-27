cae715eee9d73b64159b04e893d5129aaeae63dc
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2024-09-28 00:43:22 +0200

    Apply ruff rule RUF021
    RUF021 Parenthesize `a and b` expressions when chaining `and` and `or` together, to make the precedence clear

--- tabulate/__init__.py
+++ tabulate/__init__.py
@@ -2783,12 +2783,16 @@ class _CustomTextWrap(textwrap.TextWrapper):
                     self.max_lines is None
                     or len(lines) + 1 < self.max_lines
                     or (
-                        not chunks
-                        or self.drop_whitespace
-                        and len(chunks) == 1
-                        and not chunks[0].strip()
+                        (
+                            not chunks
+                            or (
+                                self.drop_whitespace
+                                and len(chunks) == 1
+                                and not chunks[0].strip()
+                            )
+                        )
+                        and cur_len <= width
                     )
-                    and cur_len <= width
                 ):
                     # Convert current line back to a string and store it in
                     # list of all lines (return value).
