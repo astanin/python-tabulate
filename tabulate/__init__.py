b39a162ecbb2e9898c2f759e01ddd21f4af7f316
Perry Kundert <perry@dominionrnd.com>
2024-10-07 18:01:46 -0600

    Add back in check for SEPARATING_LINE

--- tabulate/__init__.py
+++ tabulate/__init__.py
@@ -2561,9 +2561,10 @@ def _format_table(
     if rows and fmt.linebetweenrows and "linebetweenrows" not in hidden:
         # initial rows with a line below
         for row, ralign in zip(rows[:-1], rowaligns):
-            append_row(
-                lines, pad_row(row, pad), padded_widths, colaligns, fmt.datarow, rowalign=ralign
-            )
+            if row != SEPARATING_LINE:
+                append_row(
+                    lines, pad_row(row, pad), padded_widths, colaligns, fmt.datarow, rowalign=ralign
+                )
             _append_line(lines, padded_widths, colaligns, fmt.linebetweenrows)
         # the last row without a line below
         append_row(
