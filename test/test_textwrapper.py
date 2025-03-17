astanin/python-tabulate

commit e8e3091d46966f462735f227d5ef4effdfaa0582

Wordwrapping should now work when provided with a datetime a cell to wrap

test/test_textwrapper.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 """Discretely test functionality of our custom TextWrapper"""
+from __future__ import unicode_literals
 
-from tabulate import _CustomTextWrap as CTW
+import datetime
+
+from tabulate import _CustomTextWrap as CTW, tabulate
 from textwrap import TextWrapper as OTW
 
 from common import skip, assert_equal
@@ -154,4 +157,32 @@ def test_wrap_color_line_splillover():
     ]
     wrapper = CTW(width=25)
     result = wrapper.wrap(data)
+    assert_equal(expected, result)
+
+
+def test_wrap_datetime():
+    """TextWrapper: Show that datetimes can be wrapped without crashing"""
+    data = [
+        ["First Entry", datetime.datetime(2020, 1, 1, 5, 6, 7)],
+        ["Second Entry", datetime.datetime(2021, 2, 2, 0, 0, 0)],
+    ]
+    headers = ["Title", "When"]
+    result = tabulate(data, headers=headers, tablefmt="grid", maxcolwidths=[7, 5])
+
+    expected = [
+        "+---------+--------+",
+        "| Title   | When   |",
+        "+=========+========+",
+        "| First   | 2020-  |",
+        "| Entry   | 01-01  |",
+        "|         | 05:06  |",
+        "|         | :07    |",
+        "+---------+--------+",
+        "| Second  | 2021-  |",
+        "| Entry   | 02-02  |",
+        "|         | 00:00  |",
+        "|         | :00    |",
+        "+---------+--------+",
+    ]
+    expected = "\n".join(expected)
     assert_equal(expected, result)

tabulate.py
@@ -1230,7 +1230,13 @@ def _wrap_text_to_colwidths(list_of_lists, colwidths, numparses=True):
 
             if width is not None:
                 wrapper = _CustomTextWrap(width=width)
-                wrapped = wrapper.wrap(cell)
+                # Cast based on our internal type handling
+                # Any future custom formatting of types (such as datetimes)
+                # may need to be more explict than just `str` of the object
+                casted_cell = (
+                    str(cell) if _isnumber(cell) else _type(cell, numparse)(cell)
+                )
+                wrapped = wrapper.wrap(casted_cell)
                 new_row.append("\n".join(wrapped))
             else:
                 new_row.append(cell)
@@ -1862,6 +1868,7 @@ class _CustomTextWrap(textwrap.TextWrapper):
 
     def __init__(self, *args, **kwargs):
         self._active_codes = []
+        self.max_lines = None  # For python2 compatibility
         textwrap.TextWrapper.__init__(self, *args, **kwargs)
 
     @staticmethod

commit d29909b4fb17e2bbf62dde8bc730e9f29240155f

Fix handling "True"/"False" bool str and None
calling _type is incorrect here; the subsequent lines need a `str`.
Also you can't reconstruct `None` with `type(None)(None)` anyway.
This regressed in e8e3091d46966f462735f227d5ef4effdfaa0582

Fixes #305

tabulate/__init__.py
@@ -1641,13 +1641,7 @@ def _wrap_text_to_colwidths(list_of_lists, colwidths, numparses=True):
 
             if width is not None:
                 wrapper = _CustomTextWrap(width=width)
-                # Cast based on our internal type handling. Any future custom
-                # formatting of types (such as datetimes) may need to be more
-                # explicit than just `str` of the object. Also doesn't work for
-                # custom floatfmt/intfmt, nor with any missing/blank cells.
-                casted_cell = (
-                    str(cell) if _isnumber(cell) else _type(cell, numparse)(cell)
-                )
+                casted_cell = str(cell)
                 wrapped = [
                     "\n".join(wrapper.wrap(line))
                     for line in casted_cell.splitlines()

test/test_textwrapper.py
@@ -220,3 +220,27 @@ def test_wrap_datetime():
     ]
     expected = "\n".join(expected)
     assert_equal(expected, result)
+
+
+def test_wrap_optional_bool_strs():
+    """TextWrapper: Show that str bools and None can be wrapped without crashing"""
+    data = [
+        ["First Entry", "True"],
+        ["Second Entry", None],
+    ]
+    headers = ["Title", "When"]
+    result = tabulate(data, headers=headers, tablefmt="grid", maxcolwidths=[7, 5])
+
+    expected = [
+        "+---------+--------+",
+        "| Title   | When   |",
+        "+=========+========+",
+        "| First   | True   |",
+        "| Entry   |        |",
+        "+---------+--------+",
+        "| Second  | None   |",
+        "| Entry   |        |",
+        "+---------+--------+",
+    ]
+    expected = "\n".join(expected)
+    assert_equal(expected, result)