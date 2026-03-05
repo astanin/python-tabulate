ffac028f30000ee2d308b0457a0682d05eca14a7
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2026-03-05 10:10:30 +0100

    Apply ruff/flake8-implicit-str-concat rule ISC003
    Explicitly concatenated string should be implicitly concatenated

--- tabulate/__init__.py
+++ tabulate/__init__.py
@@ -300,7 +300,7 @@ def _asciidoc_row(is_header, *args):
     else:
         raise ValueError(
             "_asciidoc_row() requires two (colwidths, colaligns) "
-            + "or three (cell_values, colwidths, colaligns) arguments) "
+            "or three (cell_values, colwidths, colaligns) arguments) "
         )
 
 
@@ -1409,7 +1409,7 @@ def _prepend_row_index(rows, index):
     if isinstance(index, Sized) and len(index) != len(rows):
         raise ValueError(
             "index must be as long as the number of data rows: "
-            + f"len(index)={len(index)} len(rows)={len(rows)}"
+            f"len(index)={len(index)} len(rows)={len(rows)}"
         )
     sans_rows, separating_lines = _remove_separating_lines(rows)
     new_rows = []
