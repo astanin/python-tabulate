c327d6c41981d3668b96caeb4bab10478c263030
Sergey Astanin <s.astanin@gmail.com>
2026-03-09 17:06:48 +0100

    implement support of JSONL and CSV input formats in command line utility
    fix #173
    
    New CLI parameter:
    
    --headers HEADERS
      where HEADERS can be "firstrow", "keys", "HEADER1,HEADER2,...", "KEY1:HEADER1,KEY2:HEADER2,..."
    
    --read FILEFORMAT
      where FILEFORMAT can be "rsv" (default), "csv", "jsonl"
    
    Deprecated CLI parameter:
    
    --header
    
    Usage examples:
    
    cat ./examples/people.csv | tabulate -r csv --headers firstrow
      id  name    email              "favorite" fruit
    ----  ------  -----------------  ------------------
       1  Alice   alice@example.com  apple, kiwi
       2  Bob     bob@example.com    banana,
                                     orange,
                                     lychee
       3  Carol                      pear
    
    cat ./examples/people.jsonl | tabulate -r jsonl -f grid --headers 'id:ID,name:First Name,email:Email'
    +------+--------------+-------------------+
    |   ID | First Name   | Email             |
    +======+==============+===================+
    |    1 | Alice        | alice@example.com |
    +------+--------------+-------------------+
    |    2 | Bob          | bob@example.com   |
    +------+--------------+-------------------+

--- tabulate/__init__.py
+++ tabulate/__init__.py
@@ -2900,8 +2900,7 @@ def _main():
     """\
     Usage: tabulate [options] [FILE ...]
 
-    Pretty-print tabular data.
-    See also https://github.com/astanin/python-tabulate
+    Pretty-print tabular data. Use Python module for more features.
 
     FILE                      a filename of the file with tabular data;
                               if "-" or missing, read data from stdin.
@@ -2909,16 +2908,35 @@ def _main():
     Options:
 
     -h, --help                show this message
-    -1, --header              use the first row of data as a table header
-    -o FILE, --output FILE    print table to FILE (default: stdout)
-    -s REGEXP, --sep REGEXP   use a custom column separator (default: whitespace)
+
+    INPUT:
+    -r, --read FILEFORMAT     parse input FILEs as:
+                              rsv (REGEXP-separated values, default),
+                              csv (comma-separated valued, Excel dialect),
+                              jsonl (one JSON object per line)
+    -s REGEXP, --sep REGEXP   column separator for rsv data (default: whitespace)
+
+    FORMAT:
+    --headers HEADERS         HEADERS can be one of:
+                              "firstrow" (for csv and rsv data),
+                              "keys" (for jsonl data),
+                              "HEADER1,HEADER2,..." (for csv and rsv data),
+                              "KEY1:HEADER1,KEY2:HEADER2,..." (for jsonl data)
+    -1                        use the first row of input data as a table header
+                              (the same as --headers firstrow)
     -F FPFMT, --float FPFMT   floating point number format (default: g)
     -I INTFMT, --int INTFMT   integer point number format (default: "")
-    -f FMT, --format FMT      set output table format; supported formats:
-                              plain, simple, grid, fancy_grid, pipe, orgtbl,
-                              rst, mediawiki, html, latex, latex_raw,
-                              latex_booktabs, latex_longtable, tsv
-                              (default: simple)
+    -f FMT, --format FMT      set output table format (default: simple)
+
+    Supported output formats: asciidoc, colon_grid, double_grid, double_outline,
+    fancy_grid, fancy_outline, github, grid, heavy_grid, heavy_outline, html, jira,
+    latex, latex_booktabs, latex_longtable, latex_raw, mediawiki, mixed_grid, mixed_outline,
+    moinmoin, orgtbl, outline, pipe, plain, presto, pretty, psql, rounded_grid,
+    rounded_outline, rst, simple, simple_grid, simple_outline, textile, tsv, unsafehtml.
+
+    OUTPUT:
+    -o FILE, --output FILE    print table to FILE (default: stdout)
+                             
     """
     import getopt
 
@@ -2926,10 +2944,12 @@ def _main():
     try:
         opts, args = getopt.getopt(
             sys.argv[1:],
-            "h1o:s:F:I:f:",
+            "h1H:r:o:s:F:I:f:",
             [
                 "help",
-                "header",
+                "header",  # deprecated in CLI > 0.10
+                "headers=", # CLI > 0.10
+                "read=",   # CLI > 0.10
                 "output=",
                 "sep=",
                 "float=",
@@ -2939,7 +2959,7 @@ def _main():
             ],
         )
     except getopt.GetoptError as e:
-        print(e)
+        print(e, file=sys.stderr)
         print(usage)
         sys.exit(2)
     headers = []
@@ -2947,11 +2967,20 @@ def _main():
     intfmt = _DEFAULT_INTFMT
     colalign = None
     tablefmt = "simple"
+    fileformat = "rsv"
     sep = r"\s+"
     outfile = "-"
+    special_headers_values = ["firstrow", "keys"]
     for opt, value in opts:
         if opt in ["-1", "--header"]:
+            # "header" option is for backwards compatibility with CLI <= 0.10
+            # CLI >= 0.11 should user --headers
             headers = "firstrow"
+        if opt in ["-H", "--headers"]:
+            if value in special_headers_values:
+                headers = value
+            else:
+                headers = value  # may need to be processed
         elif opt in ["-o", "--output"]:
             outfile = value
         elif opt in ["-F", "--float"]:
@@ -2960,9 +2989,11 @@ def _main():
             intfmt = value
         elif opt in ["-C", "--colalign"]:
             colalign = value.split()
+        elif opt in ["-r", "--read"]:
+            fileformat = value.lower()
         elif opt in ["-f", "--format"]:
             if value not in tabulate_formats:
-                print("%s is not a supported table format" % value)
+                print(f"{value} is not a supported output format", file=sys.stderr)
                 print(usage)
                 sys.exit(3)
             tablefmt = value
@@ -2971,39 +3002,78 @@ def _main():
         elif opt in ["-h", "--help"]:
             print(usage)
             sys.exit(0)
+    # choose a reader and parse headers option
+    if fileformat == "rsv":
+        reader = partial(_read_rsv_file, sep=sep)
+        if type(headers) is str and headers not in special_headers_values:
+            # parse as CSV values
+            headers = headers.split(",")
+    elif fileformat == "csv":
+        reader = _read_csv_file
+        if type(headers) is str and headers not in special_headers_values:
+            # parse as CSV values
+            headers = headers.split(",")
+    elif fileformat == "jsonl":
+        reader = _read_jsonl_file
+        if not headers:
+            headers = "keys"  # reasonable default
+        if type(headers) is str and headers not in special_headers_values:
+            # "," and ":" in header titles are not supported in CLI
+            try:
+                headers2 = dict(tuple(hh.split(":",2)) for hh in headers.split(","))
+            except:
+                print(f"cannot parse headers parameter: {headers}", file=sys.stderr)
+                headers2 = []
+            headers = headers2
+    else:
+        print(f"{fileformat} is not a supported file format")
+        sys.exit(3)
+    # format all input files
     files = [sys.stdin] if not args else args
     with sys.stdout if outfile == "-" else open(outfile, "w") as out:
         for f in files:
             if f == "-":
                 f = sys.stdin
-            if _is_file(f):
-                _pprint_file(
-                    f,
+            _open_and_pprint_file(reader, f,
                     headers=headers,
                     tablefmt=tablefmt,
-                    sep=sep,
                     floatfmt=floatfmt,
                     intfmt=intfmt,
                     file=out,
                     colalign=colalign,
                 )
-            else:
-                with open(f) as fobj:
-                    _pprint_file(
-                        fobj,
-                        headers=headers,
-                        tablefmt=tablefmt,
-                        sep=sep,
-                        floatfmt=floatfmt,
-                        intfmt=intfmt,
-                        file=out,
-                        colalign=colalign,
-                    )
 
 
-def _pprint_file(fobject, headers, tablefmt, sep, floatfmt, intfmt, file, colalign):
+def _read_rsv_file(fobject, sep):
     rows = fobject.readlines()
     table = [re.split(sep, r.rstrip()) for r in rows if r.strip()]
+    return table
+
+
+def _read_jsonl_file(fobject):
+    import json
+    rows:list[str] = fobject.readlines()
+    table = [json.loads(row) for row in rows]
+    return table
+
+
+def _read_csv_file(fobject):
+    import csv
+    reader = csv.reader(fobject, dialect="excel")
+    table = [list(row) for row in reader]
+    return table
+
+
+def _open_and_pprint_file(reader, f, *args, **kwargs):
+    if _is_file(f):
+        _pprint_file(reader, f, *args, **kwargs)
+    else:
+        with open(f) as fobj:
+            _pprint_file(reader, fobj, *args, **kwargs)
+
+
+def _pprint_file(reader, fobject, headers, tablefmt, floatfmt, intfmt, file, colalign):
+    table = reader(fobject)
     print(
         tabulate(
             table,
